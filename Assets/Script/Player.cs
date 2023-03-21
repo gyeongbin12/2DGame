76ac3e3dbf92354ed322f93349d610946308d328
sesgoodboy <110909218+gyeongbin12@users.noreply.github.com>
2023-03-21 21:48:01 +0900

    3.21

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,16 +7,29 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 1.0f;
     private Vector2 direction;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.y = Input.GetAxisRaw("Vertical");
+
+        if (rigidbody2D.velocity == Vector2.zero)
+        {
+            animator.SetBool("Run", false);
+        }
+        else
+        {
+            animator.SetBool("Run", true);
+        }
         
         if(direction.x > 0)
         {
@@ -27,7 +40,13 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
-        transform.Translate(direction.normalized * speed * Time.deltaTime);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = direction.normalized
+            * speed * Time.fixedDeltaTime;
     }
 }
     
