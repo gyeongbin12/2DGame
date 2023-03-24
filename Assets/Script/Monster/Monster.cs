d238eeb7544a62796aa8a7ca19af4f5b26196476
sesgoodboy <110909218+gyeongbin12@users.noreply.github.com>
2023-03-24 21:47:53 +0900

    3.24

--- Assets/Script/Monster/Monster.cs
+++ Assets/Script/Monster/Monster.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour, IAttack
 {
-    protected int health;
     public int attack;
 
-    virtual public void Use()
+    protected int health; 
+    protected float speed;
+    protected Vector2 direction;
+    protected Transform player;
+    protected SpriteRenderer spriteRenderer;
+
+    protected virtual void Start()
+    {
+        speed = 1.0f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Character").GetComponent<Transform>();
+    }
+    
+    public void Use()
     {
         
     }
--- Assets/Script/SceneMoveManager.cs
+++ Assets/Script/SceneMoveManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneMoveManager : MonoBehaviour
@@ -19,7 +20,27 @@ public class SceneMoveManager : MonoBehaviour
 
         AsyncOperation operation = SceneManager.LoadSceneAsync(index);
 
-        yield return null;
+        progressImage.fillAmount = 0;
+
+        operation.allowSceneActivation = false;
+
+        float progress = 0;
+
+        while(!operation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, operation.progress, Time.deltaTime);
+
+            progressImage.fillAmount = progress;
+
+
+            if (progress >= 0.9f)
+            {
+                progressImage.fillAmount = 1f;
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
     }
 }
 
